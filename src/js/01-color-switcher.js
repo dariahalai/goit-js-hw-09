a8e17175c4805eb7e148901bbe83a8e3ed0979dc
Halai Daria <halaidaria@gmail.com>
2022-11-03 23:02:04 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  const refs = {
+    startBtn : document.querySelector('button[data-start]'),
+    stopBtn : document.querySelector('button[data-stop]')
+}
+let changeBG = null;
+// console.log(refs.startBtn,refs.stopBtn)
+
+refs.startBtn.addEventListener('click', letChangeBgColor);
+refs.stopBtn.addEventListener('click', stopChangeBgColor);
+
+function letChangeBgColor(){
+    changeBG = setInterval(()=>{return document.body.style.backgroundColor = getRandomHexColor() }, 1000);
+    refs.startBtn.setAttribute('disabled','');
+
+}
+
+function stopChangeBgColor(){
+    clearInterval(changeBG);
+    refs.startBtn.removeAttribute('disabled');
+
+}
+
